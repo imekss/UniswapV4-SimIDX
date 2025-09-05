999885a0efca96bd9547c866a38266ad9ea4ca3c
imekss <dgn.ousmane@gmail.com>
2025-09-05 10:25:00 +0200

    fix PoolManagerListener + API

--- listeners/test/Main.t.sol
+++ listeners/test/Main.t.sol
@@ -5,5 +5,14 @@ import {Test, console} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
 import "sim-idx-sol/Simidx.sol";
 import "sim-idx-generated/Generated.sol";
+import { PoolManagerListener } from "../src/PoolManagerListener.sol";
 
 // In this file you can implement unit tests for your listeners.
+// contract PoolManagerListenerTest is Test {
+//     PoolManagerListener public listener;
+
+//     function setUp() public {
+//         listener = new PoolManagerListener();
+//         vm.recordLogs();
+//     }
+// }
